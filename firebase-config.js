5ad4300c265a1ad91d033b62a72333c537aeaa9e
bida24-067 <bida24-067@thuto.bac.ac.bw>
2025-04-29 10:46:14 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// filepath: firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCqy5--P3iowffUMJ_cHaNewLpLCtPgM3s",
+  authDomain: "yourwear-b94dc.firebaseapp.com",
+  projectId: "yourwear-b94dc",
+  storageBucket: "yourwear-b94dc.appspot.com",
+  messagingSenderId: "1076578676354",
+  appId: "1:1076578676354:web:f053ab2fea80a2485d4e9b",
+  measurementId: "G-C59RWHLQZG"
+};
+firebase.initializeApp(firebaseConfig);
